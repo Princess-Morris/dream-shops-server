3bacd8312470a3dea05ba2dd9310d71c0b648ca6
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-05-29 16:07:45 +0100

    mapped entities to table in database

--- src/main/java/com/princess/dream_shops/model/Image.java
+++ src/main/java/com/princess/dream_shops/model/Image.java
@@ -3,12 +3,35 @@ package com.princess.dream_shops.model;
 import java.sql.Blob;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String fileName;
     private String fileType;
+    
+    @Lob
     private Blob image;
     private String downloadUrl;
 
-    private List<Product> products;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 }
