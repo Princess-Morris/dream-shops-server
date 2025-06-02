9c4b86bb5beb47cc0e0e2dcbc35e000c9d1aeeb8
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-02 15:27:15 +0100

    implement service class to create and update images

---
+++ src/main/java/com/princess/dream_shops/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.princess.dream_shops.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.princess.dream_shops.model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+}
--- src/main/java/com/princess/dream_shops/service/image/IImageService.java
+++ src/main/java/com/princess/dream_shops/service/image/IImageService.java
@@ -1,12 +1,15 @@
 package com.princess.dream_shops.service.image;
 
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
 
+import com.princess.dream_shops.ImageDto;
 import com.princess.dream_shops.model.Image;
 
 public interface IImageService {
     Image getImageById(Long id);
     void deleteImageById(Long id);
-    Image saveImage(MultipartFile file, Long productId);
-    void updateImage(MultipartFile file, Long productId);
+    List<ImageDto> saveImages(List<MultipartFile> files, Long productId);
+    void updateImage(MultipartFile file, Long imageId, Long productId);
 }
