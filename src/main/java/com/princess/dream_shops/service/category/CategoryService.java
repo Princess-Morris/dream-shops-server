0e298fe500b4addcfe5acb4b01cc6cb17cbb61de
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-02 11:37:16 +0100

    creating interface for category service

--- src/main/java/com/princess/dream_shops/service/category/CategoryService.java
+++ src/main/java/com/princess/dream_shops/service/category/CategoryService.java
@@ -1,5 +1,34 @@
 package com.princess.dream_shops.service.category;
 
-public class CategoryService {
+import java.util.List;
 
+import com.princess.dream_shops.model.Category;
+
+public class CategoryService implements iCategoryService {
+
+    @Override
+   public Category getCategoryById(Long id){
+        return null;
+    }
+
+    @Override
+    public Category getCategoryByName(String name){
+        return null;
+    }
+
+    @Override
+    public List<Category>getAllCategories(){
+        return List.of();
+    }
+
+    public Category addCategory(Category category){
+         return null;
+    }
+
+    public Category updateCategory(Category category){
+        return null;
+    }
+
+    public void deleteCategoryById(Long id){
+    }
 }
