cd700cf22dbad9b4bbbc9ec13257fb55f87926d7
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-02 18:56:42 +0100

    implement category controller to get all categories

--- src/main/java/com/princess/dream_shops/controller/CategoryController.java
+++ src/main/java/com/princess/dream_shops/controller/CategoryController.java
@@ -1,5 +1,39 @@
 package com.princess.dream_shops.controller;
 
-public class CategoryController {
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.princess.dream_shops.model.Category;
+import com.princess.dream_shops.response.ApiResponse;
+import com.princess.dream_shops.service.category.iCategoryService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
+public class CategoryController {
+     private final iCategoryService categoryService;
+
+     @GetMapping("/all")
+     public String getMethodName(@RequestParam String param) {
+         return new String();
+     }
+     
+     public ResponseEntity<ApiResponse> getAllCategories(){
+        try{
+             List<Category> categories = categoryService.getAllCategories();
+             return ResponseEntity.ok(new ApiResponse("Found!", categories));
+        } catch (Exception e){
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Error", INTERNAL_SERVER_ERROR));
+        }
+     }
 }
