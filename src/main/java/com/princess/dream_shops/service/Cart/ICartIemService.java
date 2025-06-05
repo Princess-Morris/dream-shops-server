d37fa62d5452fa790571342805b55390eaef9050
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-05 19:12:58 +0100

    update: Cart service interface  -added 3 interfaces for cart item

--- src/main/java/com/princess/dream_shops/service/Cart/ICartIemService.java
+++ src/main/java/com/princess/dream_shops/service/Cart/ICartIemService.java
@@ -3,4 +3,7 @@ package com.princess.dream_shops.service.Cart;
 import com.princess.dream_shops.model.Cart;
 
 public interface ICartIemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
 }
