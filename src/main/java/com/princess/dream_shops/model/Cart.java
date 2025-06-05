b2ec46598ae16c6d52d31f62f1c85b2204de9443
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-05 16:44:42 +0100

    Feat: Model: -created two models:   -CartItem ad   -Cart

---
+++ src/main/java/com/princess/dream_shops/model/Cart.java
@@ -0,0 +1,11 @@
+package com.princess.dream_shops.model;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public class Cart {
+    private Long id;
+    private BigDecimal totalAmount;
+    private Set<CartItem> cartItems;
+
+}
---
+++ src/main/java/com/princess/dream_shops/model/CartItem.java
@@ -0,0 +1,5 @@
+package com.princess.dream_shops.model;
+
+public class CartItem {
+
+}
