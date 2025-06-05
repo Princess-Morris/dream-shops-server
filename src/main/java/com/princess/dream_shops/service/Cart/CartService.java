a7ef841644397089e1361eed0dfd48374e799abf
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-05 18:58:53 +0100

    Update: Cart service  -updated method to get total price of items in a cart

--- src/main/java/com/princess/dream_shops/service/Cart/CartService.java
+++ src/main/java/com/princess/dream_shops/service/Cart/CartService.java
@@ -40,9 +40,11 @@ public class CartService implements ICartService {
     @Override
     public BigDecimal getTotalPrice(Long id){
         Cart cart = getCart(id);
-         return cart.getItems()
-         .stream()
-         .map(CartItem :: getTotalPrice)
-         .reduce(BigDecimal.ZERO, BigDecimal::add);
+        //  return cart.getItems()
+        //  .stream()
+        //  .map(CartItem :: getTotalPrice)
+        //  .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return cart.getTotalAmount();
     }
 }
