7ded5be16423fb906bd67545593b11dd1810e3a5
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-05 19:17:27 +0100

    Update: Cart service -implement method for cart service interface

--- src/main/java/com/princess/dream_shops/service/Cart/CartItemService.java
+++ src/main/java/com/princess/dream_shops/service/Cart/CartItemService.java
@@ -1,5 +1,20 @@
 package com.princess.dream_shops.service.Cart;
 
-public class CartItemService {
+public class CartItemService implements ICartIemService {
+
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity){
+
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId){
+
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity){
+
+    }
 
 }
