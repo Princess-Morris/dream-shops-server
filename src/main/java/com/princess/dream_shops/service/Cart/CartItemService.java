a265de01a95e81251bd5c4f0c065b1ac2950daba
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-05 19:25:55 +0100

    steps to add an item to cart

--- src/main/java/com/princess/dream_shops/service/Cart/CartItemService.java
+++ src/main/java/com/princess/dream_shops/service/Cart/CartItemService.java
@@ -1,10 +1,26 @@
 package com.princess.dream_shops.service.Cart;
 
+import org.springframework.stereotype.Service;
+
+import com.princess.dream_shops.repository.CartItemRepository;
+import com.princess.dream_shops.service.product.iProductService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class CartItemService implements ICartIemService {
+
+    private final CartItemRepository cartItemRepository;
+    private final iProductService productService;
 
     @Override
     public void addItemToCart(Long cartId, Long productId, int quantity){
-
+        //1. Get the cart
+        // 2. Get the product
+        //3. Check if the product is already in the requested quantity
+        //4. If yes, then increase the quantity with the requested quanity
+        // 5. If No, then initiate a new CartItem entry.
     }
 
     @Override
