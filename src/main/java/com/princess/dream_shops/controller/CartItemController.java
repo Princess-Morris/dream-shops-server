670332646525c5a98f021fca2bfda8dd04f1af9e
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-06 14:02:32 +0100

    Update:  Cart item controller  -implement endpoint to add item to cart

--- src/main/java/com/princess/dream_shops/controller/CartItemController.java
+++ src/main/java/com/princess/dream_shops/controller/CartItemController.java
@@ -1,13 +1,37 @@
 package com.princess.dream_shops.controller;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.princess.dream_shops.exceptions.ResourceNotFoundException;
+import com.princess.dream_shops.response.ApiResponse;
+import com.princess.dream_shops.service.Cart.ICartIemService;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/cartItems")
 public class CartItemController {
+    private final ICartIemService cartIemService;
 
+    @PostMapping("/item/add")
+    public ResponseEntity<ApiResponse> addItemToCart(@RequestParam Long cartId,
+                                                    @RequestParam Long productId,
+                                                    @RequestParam Integer quantity){
+      try{
+        cartIemService.addItemToCart(cartId, productId, quantity);  
+        return ResponseEntity.ok(new ApiResponse("Add Item Success", null));
+      } catch (ResourceNotFoundException e){
+        return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+      }
+                                                      
+ }
 }
