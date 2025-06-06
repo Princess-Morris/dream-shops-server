2467ed44f1c92870bb5f1b39832b6997e879e865
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-06 11:53:37 +0100

    Update: Cart-item-service  -implemented method to add item to the cart

--- src/main/java/com/princess/dream_shops/service/Cart/CartItemService.java
+++ src/main/java/com/princess/dream_shops/service/Cart/CartItemService.java
@@ -2,7 +2,12 @@ package com.princess.dream_shops.service.Cart;
 
 import org.springframework.stereotype.Service;
 
+import com.princess.dream_shops.model.Cart;
+import com.princess.dream_shops.model.CartItem;
+import com.princess.dream_shops.model.Product;
 import com.princess.dream_shops.repository.CartItemRepository;
+import com.princess.dream_shops.repository.CartRepository;
+import com.princess.dream_shops.service.category.iCategoryService;
 import com.princess.dream_shops.service.product.iProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,6 +18,8 @@ public class CartItemService implements ICartIemService {
 
     private final CartItemRepository cartItemRepository;
     private final iProductService productService;
+    private final ICartService cartService;
+    private final CartRepository cartRepository;
 
     @Override
     public void addItemToCart(Long cartId, Long productId, int quantity){
@@ -21,6 +28,27 @@ public class CartItemService implements ICartIemService {
         //3. Check if the product is already in the requested quantity
         //4. If yes, then increase the quantity with the requested quanity
         // 5. If No, then initiate a new CartItem entry.
+
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getItems()
+        .stream()
+        .filter(item -> item.getProduct().getId().equals(productId))
+        .findFirst()
+        .orElse(new CartItem()); 
+        if(cartItem.getId() == null){
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        }
+        else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
     }
 
     @Override
