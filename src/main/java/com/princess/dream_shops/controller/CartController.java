eaa7e8883fd40fa351235b39e35e1c8c239c66c8
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-06 13:22:23 +0100

    Update: Cart controller  -implement endpoint to get a given cart

--- src/main/java/com/princess/dream_shops/controller/CartController.java
+++ src/main/java/com/princess/dream_shops/controller/CartController.java
@@ -1,16 +1,41 @@
 package com.princess.dream_shops.controller;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.princess.dream_shops.exceptions.ResourceNotFoundException;
+import com.princess.dream_shops.model.Cart;
+import com.princess.dream_shops.response.ApiResponse;
 import com.princess.dream_shops.service.Cart.ICartService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${api.prefix}/carts")
 public class CartController {
     private final ICartService cartService; 
+
+    @GetMapping("/{cartId}/my-cart")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId){
+        try{
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Success", cart));
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+
+    }
 
 }
