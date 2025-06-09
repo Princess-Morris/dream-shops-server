2fae821f39a45bbe0db6b5ff8b328e1aecde042a
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-06-09 10:06:30 +0100

    Update Cart service -implement method to generate a cart Id '

--- src/main/java/com/princess/dream_shops/service/Cart/CartService.java
+++ src/main/java/com/princess/dream_shops/service/Cart/CartService.java
@@ -1,6 +1,7 @@
 package com.princess.dream_shops.service.Cart;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ public class CartService implements ICartService {
 
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final AtomicLong cartIdGenerator = new AtomicLong(0);
 
     @Override
     public Cart getCart(Long id){
@@ -46,5 +48,12 @@ public class CartService implements ICartService {
         //  .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         return cart.getTotalAmount();
+    }
+
+    public Long initializeNewCart(){
+        Cart newCart = new Cart();
+        Long newCartId = cartIdGenerator.incrementAndGet();
+        newCart.setId(newCartId);
+        return cartRepository.save(newCart).getId();
     }
 }
